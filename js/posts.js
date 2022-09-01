d90e8a3fac71a9cffd068eec5e2a6b5bb3a2f84a
Razib2019 <hafizurrahmanrazib@gmail.com>
2022-09-02 03:20:51 +0600

    upto post.js done

---
+++ js/posts.js
@@ -0,0 +1,30 @@
+function loadPosts() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(res => res.json())
+        .then(data => displayPosts(data))
+
+}
+
+/* 
+1. Get the container element where you want to add the new element
+2. Create Child Element
+3. Set innerText or innerHTML
+4. Append Child
+
+*/
+
+function displayPosts(posts) {
+    const postsContainer = document.getElementById('posts-container');
+    for (const post of posts) {
+        const postDiv = document.createElement('div');
+        postDiv.classList.add('post');
+        postDiv.innerHTML = `
+            <h4>User- ${post.userId}</h4>
+            <h5>Post: ${post.title}</h5>
+            <p>Post Description: ${post.body}</p>
+        `;
+        postsContainer.appendChild(postDiv);
+    }
+}
+
+loadPosts();
